4d0fea5fabec7a5e60976eee7c9bd48b841891c3
Aditya Prakash Acharya <131473291+ShaqDaddyy@users.noreply.github.com>
2023-05-12 20:31:29 +0530

    A2Q1.java

--- Assignment2/src/A2Q1.java
+++ Assignment2/src/A2Q1.java
@@ -1,3 +1,20 @@
+//A phone number can be thought of as having three parts: the area code, the exchange code and 
+the number
+Example A phone number, such as (212) 767-8900, can be thought of as having three parts: 
+the area code (212), the exchange (767) and the number (8900).
+Define a class Phone to store these three parts of a phone number separately as instance 
+variable (area_code, exchange, number). The class consists of two member methods: input ( )
+and display ( ). Create two class objects of type phone. Initialize one directly (by using dot (.) 
+operator and instance variable with value area_code: 212, exchange: 767, 
+number: 8900), and initialize other from the user through member method input ( ). Display 
+both the numbers.
+Sample Run:
+My number is (212) 767-8900
+Your number is (415) 555-1212//
+
+
+
+
 import java.util.Scanner;
 public class A2Q1 {
     int areacode, exchange, number;
