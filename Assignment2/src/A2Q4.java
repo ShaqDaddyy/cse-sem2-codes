945aeb49412a96d8fc33e3d6b2f401fca101078f
Aditya Prakash Acharya <131473291+ShaqDaddyy@users.noreply.github.com>
2023-05-12 20:35:28 +0530

    A2Q4.java

--- Assignment2/src/A2Q4.java
+++ Assignment2/src/A2Q4.java
@@ -1,3 +1,11 @@
+/*Define a class called product. Two instance variables of the class are pid (product-id) and 
+price (product price). It consists of one static variable tot_price (total price). Initialize the value 
+of instance variables through parameterized constructor. It consists of a display ( ) method to 
+display the value of instance variables. A person went to market and purchase 5 different 
+products. Using the above mentioned class, display the details of products that the person has 
+purchased. Also, determine how much total amount the person will pay for the purchase of 5 
+products.*/
+
 import java.util.Scanner;
 class Product{
 	int pid;
