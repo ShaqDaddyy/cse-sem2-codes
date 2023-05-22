69b8a2b8e90226ffeabc18fc09bf634a5447d843
Aditya Prakash Acharya <131473291+ShaqDaddyy@users.noreply.github.com>
2023-05-22 09:23:11 +0530

    HA1Q2.java

--- Assignment1/src/HA1Q2.java
+++ Assignment1/src/HA1Q2.java
@@ -1,3 +1,6 @@
+/*Write a Java program that takes all the lines input to standard input and writes them to
+standard output in reverse order. That is, each line is output in the correct order, but the
+ordering of the lines is reversed.*/
 import java.util.Scanner;
 public class HA1Q2 {
 
