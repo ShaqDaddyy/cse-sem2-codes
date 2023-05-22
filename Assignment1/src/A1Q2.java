c3f48cc4b6b777b2e6e79584922601fe2a5aeaa1
Aditya Prakash Acharya <131473291+ShaqDaddyy@users.noreply.github.com>
2023-05-22 09:16:14 +0530

    A1Q2.java

--- Assignment1/src/A1Q2.java
+++ Assignment1/src/A1Q2.java
@@ -1,3 +1,13 @@
+/*The body mass index (BMI) is commonly used by health and nutrition professionals to estimate 
+human body fat in populations. It is computed by taking the individual's weight (mass) in 
+kilograms and dividing it by the square of their height in meters. i.e.
+Metric: BMI = 𝑾𝒆𝒊𝒈𝒉𝒕 (𝒌𝒈) (𝑯𝒆𝒊𝒈𝒉𝒕)𝟐 (𝒎𝟐)
+Write a java program by using conditional statement to show the category for a given BMI.
+Category BMI
+Less than 18.5 Underweight
+18.5 to 24.9 Normal Weight
+25.0 to 29.9 Overweight
+30.0 or more Obese*/
 import java.util.Scanner;
 public class A1Q2 {
 
