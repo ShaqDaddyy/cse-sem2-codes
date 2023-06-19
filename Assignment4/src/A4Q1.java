90bea4210d4a55419ff9e7a30888606261477789
Aditya Prakash Acharya <131473291+ShaqDaddyy@users.noreply.github.com>
2023-06-19 19:33:51 +0530

    A4Q1.java

--- Assignment4/src/A4Q1.java
+++ Assignment4/src/A4Q1.java
@@ -36,12 +36,48 @@ public class A4Q1 {
     		System.out.println("THANK YOU");
     	}
     }
+	public static void display(Node temp){
+		while(temp != null){
+			System.out.println(temp.regd_no + " " + temp.mark);
+			temp = temp.next;
+		}
+	}
+
+	public static int count(Node temp) {
+		int c = 0;
+		while(temp != null) {
+			c++;
+			temp = temp.next;
+		}
+		return c;
+	}
+	public static void search(Node temp) {
+		int pos = 0;
+		System.out.println("Enter your regd no to increase the marks");
+		int r = sc.nextInt();
+		while(temp != null) {
+			if(temp.regd_no == r {
+				System.out.println("enter the marks to increase");
+				int m = sc.nextInt();
+				temp.mark = temp.mark + m;
+				pos = 1;
+			}
+			temp = temp.next;
+		}
+		if(pos == 0) {
+			System.out.println("Student info is not available");
+		}
+	}
+	
 	public static void main(String[] args) {
 	int choice;
 	while(true) {
 		System.out.println("*****MENU*****");
-		System.out.println("0:EXIT");
-		System.out.println("1:Create");
+		System.out.println("0 : EXIT");
+		System.out.println("1 : Create");
+		System.out.println("2 : display");
+		System.out.println("3 : count nodes ");
+		System.out.println("4 : search element");
 		System.out.println("Enter the choice");
 		choice = sc.nextInt();
 		switch(choice) {
@@ -50,6 +86,16 @@ public class A4Q1 {
 		case 1:
 			Node p = null;
 			create(p);
+		case 2:
+			display(start);
+			break;
+		case 3:	
+			int c = sc.nextInt();
+			System.out.println("No of nodes are : " + c);
+			break;
+		case 4:
+			search(start);
+			break;
 		default:
 			System.out.println("WRONG CHOICE");
 	    }
