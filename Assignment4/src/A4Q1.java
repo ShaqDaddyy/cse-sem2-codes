936db7f3d5c8da56c0286a578469f702a39deef8
Aditya Prakash Acharya <131473291+ShaqDaddyy@users.noreply.github.com>
2023-07-09 10:40:38 +0530

    A4Q1.java

--- Assignment4/src/A4Q1.java
+++ Assignment4/src/A4Q1.java
@@ -68,6 +68,119 @@ public class A4Q1 {
 			System.out.println("Student info is not available");
 		}
 	}
+	 public static Node ins_Beg(Node q) {
+    	q = new Node();
+    	System.out.print("Enter the your regd_no : ");
+    	q.regd_no = sc.nextInt();
+    	System.out.print("Enter the mark : ");
+    	q.mark = sc.nextFloat();
+    	q.next = start;
+    	return q;
+    }
+    
+    public static void insEnd(Node temp) {
+    	Node q = new Node();
+    	System.out.print("Enter the your regd_no : ");
+    	q.regd_no = sc.nextInt();
+    	System.out.print("Enter the mark : ");
+        q.mark = sc.nextFloat();
+        q.next = null;
+        if(start == null)
+        	start = q;
+        else
+        	
+        while(temp.next != null) {
+        	temp = temp.next;
+        	temp.next = q;
+        }
+    }
+    public static void DelBeg() {
+    	if(start == null) {
+    		System.out.println("underflow");
+    		return;
+    	}
+    	start = start.next;
+    }
+    
+    public static void delEnd() {
+        if (start == null) {
+            System.out.println("Underflow: Linked list is empty.");
+            return;
+        }
+
+        Node temp = start;
+        Node prev = null;
+
+        while (temp.next != null) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if (prev == null) {
+            start = null; 
+        } else {
+            prev.next = null;
+        }
+
+        System.out.println("Last node deleted from the list.");
+    }
+    
+    public static void insLoc() {
+    	System.out.println("Enter the node location");
+    	int loc = sc.nextInt();
+    	int c1 = count(start);
+    	if(loc <= c1++) {
+    		if(loc == 1) {
+    			start = ins_Beg(start);
+    		}
+    		else if(loc == c1 +1)
+    		    insEnd(start);
+    		else {
+    			Node q = new Node();
+    			q.regd_no = sc.nextInt();
+    	    	System.out.print("Enter the mark : ");
+    	        q.mark = sc.nextFloat();
+    	        Node temp = start;
+    	        int cnt = 1;
+    	        while(cnt < loc - 1) {
+    	        	cnt++;
+    	        	temp = temp.next;
+    	        }
+    	        
+    	        temp.next = temp.next.next;
+    		}
+    	}	
+    }
+    
+    public static void delLoc(Node start) {
+        if (start == null) {
+            System.out.println("Underflow: Linked list is empty.");
+            return;
+        }
+
+        System.out.println("Enter the node location");
+        int loc = sc.nextInt();
+        int c = count(start);
+
+        if (loc <= c) {
+            if (loc == 1) {
+                DelBeg();
+            } else {
+                Node temp = start;
+                int cnt = 1;
+
+                while (cnt < loc - 1) {
+                    temp = temp.next;
+                    cnt++;
+                }
+
+                temp.next = temp.next.next;
+                System.out.println("Node at location " + loc + " deleted from the list.");
+            }
+        } else {
+            System.out.println("Invalid location.");
+        }
+    }
 	
 	public static void main(String[] args) {
 	int choice;
@@ -78,6 +191,12 @@ public class A4Q1 {
 		System.out.println("2 : display");
 		System.out.println("3 : count nodes ");
 		System.out.println("4 : search element");
+		System.out.println("5 : Insert a node at begining");
+		System.out.println("6 : insert a node at end");
+		System.out.println("7 : delete a node at Begining");
+		System.out.println("8 : delete a node at end");
+		System.out.println("9 : insert a node at any loc");
+		System.out.println("10 : delete a node at any location");
 		System.out.println("Enter the choice");
 		choice = sc.nextInt();
 		switch(choice) {
@@ -95,6 +214,24 @@ public class A4Q1 {
 			break;
 		case 4:
 			search(start);
+			break;
+		case 5 :
+			ins_Beg(start);
+			break;
+		case 6 :
+			insEnd(start);
+			break;
+		case 7 :
+			DelBeg();
+			break;
+		case 8 :
+			delEnd();
+			break;
+		case 9 :
+			insLoc();
+			break;
+		case 10 :
+			delLoc(start);
 			break;
 		default:
 			System.out.println("WRONG CHOICE");
